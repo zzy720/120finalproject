e705572207486d7f44c6370a5c70af609d4b60ce
Zeyu Zhang <49921700+zzhan255@users.noreply.github.com>
2021-05-31 18:06:02 -0700

    mroe pixel
    1

---
+++ assets/tilemaps/sky.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="sky" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../puzzle2/sky.png" width="1024" height="1024"/>
+</tileset>
